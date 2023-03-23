a42e21aa2100c4295cb214aa3159d33437db43d5
Yabel <yabelrr@gmail.com>
2023-03-23 11:42:11 +0100

    Removed Socials and Scroll down, changing image and content display. Working on margins next

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./About.css";
 import ME from "../../assets/foto-yabel.JPG";
 import CTA from "../header/CTA.jsx"; // 7.0 import the CTA component and use it
-import HeaderSocial from "../header/HeaderSocials.jsx";
+// import HeaderSocial from "../header/HeaderSocials.jsx";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
@@ -16,13 +16,7 @@ const About = () => {
       <h2>Yabel Rodríguez</h2>
       <div className="container about__container">
 
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={ME} alt="" />
-            <h2>Yabel Rodriguez<br></br><span>Full Stack Developer</span></h2>
-            <a href="# ">Hire me</a>
-          </div>
-        </div>
+       
 
         <div className="about__content">
           <div className="about__cards">
@@ -52,11 +46,19 @@ const About = () => {
 
           {/* <a href="#contact" className="btn btn-primary">Let's Talk</a> */}
           <CTA />
-          <HeaderSocial />
+          {/* <HeaderSocial /> */}
 
-          <a href="#contact" className="scroll__down">
+          {/* <a href="#contact" className="scroll__down">
             Scroll Down
-          </a>
+          </a> */}
+        </div>
+
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+            <h2>Yabel Rodriguez<br></br><span>Full Stack Developer</span></h2>
+            <a href="# ">Hire me</a>
+          </div>
         </div>
       </div>
     </section>
